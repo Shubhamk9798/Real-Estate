51f995f707cd4b376eae7adcd95206401abdf2fb
Shubham9798 <ashubhamk1997@gmail.com>
2024-01-07 12:40:36 +0530

    first commit

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,43 +1,54 @@
-import { useContext } from "react";
+import React from "react";
 import Image from "next/image";
-import { Box, Icon, Flex } from "@chakra-ui/react";
-import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Box } from "@chakra-ui/react";
 
-const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
+const ImageScrollbar = ({ data }) => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1023, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 750, min: 0 },
+      items: 1,
+      width: 200,
+      height: 100,
+    },
+  };
 
   return (
-    <Flex justifyContent="center" alignItems="center" marginRight="1"></Flex>
+    <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
+      <Carousel responsive={responsive}>
+        {data.map((item) => (
+          <Box
+            width="1900px"
+            height="700px"
+            key={item.id}
+            itemId={item.id}
+            overflow="hidden"
+            p="0"
+          >
+            <Image
+              className=""
+              alt="property"
+              placeholder="blur"
+              blurDataURL={item.url}
+              src={item.url}
+              width={1200}
+              height={700}
+              sizes="(max-width: 500px) 1000px, (max-width: 1023px) 4000px, 1000px"
+            />
+          </Box>
+        ))}
+      </Carousel>
+    </div>
   );
 };
-const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
 
-  return (
-    <Flex justifyContent="center" alignItems="center" marginLeft="1"></Flex>
-  );
-};
-
-const ImageScrollbar = ({ data }) => (
-  <ScrollMenu
-    LeftArrow={LeftArrow}
-    RightArrow={RightArrow}
-    style={{ overflow: "hidden" }}
-  >
-    {data.map((item) => (
-      <Box width="910px" key={item.id} itemId={item.id} overflow="hidden" p="1">
-        <Image
-          alt="property"
-          placeholder="blur"
-          blurDataURL={item.url}
-          src={item.url}
-          width={1000}
-          height={500}
-          sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
-        />
-      </Box>
-    ))}
-  </ScrollMenu>
-);
 export default ImageScrollbar;
